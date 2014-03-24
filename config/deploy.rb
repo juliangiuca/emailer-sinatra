f8c59cb36a1043b86c97fba40c651cdf6a8ba285
juliangiuca <julian.giuca@gmail.com>
2014-03-23 18:10:00 -0700

    again

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,6 +16,12 @@ set :repo_url, 'git@github.com:juliangiuca/emailer.git'
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 set :keep_releases, 3
 
+set :rbenv_type, :user # or :system, depends on your rbenv setup
+set :rbenv_ruby, '2.1.1'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all # default value
+
 namespace :deploy do
 
   desc 'Restart application'
